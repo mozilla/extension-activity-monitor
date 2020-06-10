936ce276cd5c20485cca7672c4efbdddca6d48c6
atiqueahmedziad <softfilebd@gmail.com>
2020-06-10 23:33:39 +0600

    feat: save and load module added

--- src/lib/Dummy.js
+++
@@ -1,5 +0,0 @@
-export default class Dummy {
-  setname(name) {
-    return name;
-  }
-}
--- tests/dummy.test.js
+++
@@ -1,6 +0,0 @@
-import Dummy from '../src/lib/Dummy';
-
-test('just a dummy test', () => {
-  const dummy = new Dummy();
-  expect(dummy.setname('john doe')).toBe('john doe');
-});
