mozilla/extension-activity-monitor

commit 913e4a61507770fc34c49441c58d6be757aa51d7

feat: filter option with timestamp in context menu (#17)

src/lib/web-component/log-view/log-view-element.js
@@ -60,18 +60,6 @@ export class LogView extends HTMLElement {
     this.tableBody.appendChild(rowsFragment);
   }
 
-  openDetailSidebar(logDetails) {
-    const logString = JSON.stringify(logDetails, null, 2);
-    this.logDetails.textContent = logString;
-    this.logTableWrapper.classList.add('width-60');
-    this.logDetailWrapper.hidden = false;
-  }
-
-  closeDetailSidebar() {
-    this.logDetailWrapper.hidden = true;
-    this.logTableWrapper.classList.remove('width-60');
-  }
-
   handleEvent(event) {
     if (event.type === 'click') {
       const logDetails = event.target.closest('tr')?._log;
@@ -84,7 +72,42 @@ export class LogView extends HTMLElement {
       if (event.target === this.closeBtn) {
         this.closeDetailSidebar();
       }
+    } else if (event.type === 'contextmenu') {
+      // This event can only be triggered in table body.
+      browser.menus.overrideContext({});
+      this.createContextMenu();
+      browser.menus.refresh();
     }
+  }
+
+  openDetailSidebar(logDetails) {
+    const logString = JSON.stringify(logDetails);
+    this.logDetails.textContent = logString;
+    this.logTableWrapper.classList.add('width-60');
+    this.logDetailWrapper.hidden = false;
+  }
+
+  closeDetailSidebar() {
+    this.logDetailWrapper.hidden = true;
+    this.logTableWrapper.classList.remove('width-60');
+  }
+
+  createContextMenu() {
+    browser.menus.create({
+      id: 'startTime',
+      title: 'Start from this timestamp',
+      contexts: ['page'],
+      viewTypes: ['tab'],
+      documentUrlPatterns: [location.href],
+    });
+
+    browser.menus.create({
+      id: 'stopTime',
+      title: 'Stop at this timestamp',
+      contexts: ['page'],
+      viewTypes: ['tab'],
+      documentUrlPatterns: [location.href],
+    });
   }
 
   clearTable() {
@@ -93,11 +116,13 @@ export class LogView extends HTMLElement {
 
   connectedCallback() {
     this.tableBody.addEventListener('click', this);
+    this.tableBody.addEventListener('contextmenu', this);
     this.closeBtn.addEventListener('click', this);
   }
 
   disconnectedCallback() {
     this.tableBody.removeEventListener('click', this);
+    this.tableBody.removeEventListener('contextmenu', this);
     this.closeBtn.removeEventListener('click', this);
   }
 }

src/activitylog/activitylog.css
@@ -25,7 +25,7 @@
   color: #ffffff;
 }
 
-.filter-wrapper {
+.filter-boxes {
   display: flex;
   margin: 10px 0;
 }

src/activitylog/activitylog.html
@@ -12,6 +12,10 @@
     ></script>
     <script
       type="module"
+      src="/lib/web-component/filter-timestamp/filter-timestamp-element.js"
+    ></script>
+    <script
+      type="module"
       src="/lib/web-component/filter-option/filter-option-element.js"
     ></script>
     <script
@@ -28,25 +32,28 @@
       <div class="activity-log-wrapper">
         <h2>Activity Logs</h2>
         <div class="filter-wrapper">
-          <filter-keyword></filter-keyword>
-          <filter-option filter-key="id">
-            Select extensions to filter logs
-          </filter-option>
-          <filter-option
-            filter-key="viewType"
-            title="This filter applies to all log entries except content scripts."
-          >
-            Select view type to filter logs
-          </filter-option>
-          <filter-option filter-key="type">
-            Select api type to filter logs
-          </filter-option>
-          <filter-option
-            filter-key="name"
-            title="This filter only applies to log entries related to API events."
-          >
-            Select api name to filter logs
-          </filter-option>
+          <div class="filter-boxes">
+            <filter-keyword></filter-keyword>
+            <filter-option filter-key="id">
+              Select extensions to filter logs
+            </filter-option>
+            <filter-option
+              filter-key="viewType"
+              title="This filter applies to all log entries except content scripts."
+            >
+              Select view type to filter logs
+            </filter-option>
+            <filter-option filter-key="type">
+              Select api type to filter logs
+            </filter-option>
+            <filter-option
+              filter-key="name"
+              title="This filter only applies to log entries related to API events."
+            >
+              Select api name to filter logs
+            </filter-option>
+          </div>
+          <filter-timestamp></filter-timestamp>
         </div>
         <log-view></log-view>
       </div>
@@ -113,4 +120,27 @@
       </div>
     </template>
   </body>
+
+  <template id="filterTimestampTemplate">
+    <link
+      rel="stylesheet"
+      href="/lib/web-component/filter-timestamp/filter-timestamp.css"
+    />
+    <div class="timestamp-filter-container" hidden>
+      <div class="label">
+        Timestamp Filter Applied
+        <div id="clearBtn" title="Clear timestamp filter"></div>
+      </div>
+      <div class="timestamp-filter-options">
+        <div class="start-time-container">
+          <div class="title">Start Time:</div>
+          <div id="startTimeLabel">From beginning</div>
+        </div>
+        <div class="stop-time-container">
+          <div class="title">Stop Time:</div>
+          <div id="stopTimeLabel">Up to End</div>
+        </div>
+      </div>
+    </div>
+  </template>
 </html>

src/icons/circle-close.svg
@@ -1,0 +1,1 @@
+<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x-circle"><circle cx="12" cy="12" r="10"></circle><line x1="15" y1="9" x2="9" y2="15"></line><line x1="9" y1="9" x2="15" y2="15"></line></svg>

src/lib/ext-activitylog.js
@@ -9,6 +9,7 @@ class Model {
       type: new Set(),
       name: new Set(),
       keyword: '',
+      timeStamp: null,
     };
   }
 
@@ -27,6 +28,10 @@ class Model {
    * includes api_call, api_event, content_script, user_script.
    * @param {Set<string>} [updateFilter.name] - It contains API names only.
    * @param {string} [updateFilter.keyword]
+   * @param {null|object} [updateFilter.timeStamp] - It is null when timestamp
+   * filter is not applied.
+   * @param {number} [updateFilter.timeStamp.start]
+   * @param {number} [updateFilter.timeStamp.stop]
    */
   setFilter(updateFilter) {
     Object.assign(this.filter, updateFilter);
@@ -38,7 +43,8 @@ class Model {
       this.matchFilterViewType(log) &&
       this.matchFilterType(log.type) &&
       this.matchFilterApiName(log) &&
-      this.matchFilterKeyword(log.data)
+      this.matchFilterKeyword(log.data) &&
+      this.matchFilterTimestamp(log.timeStamp)
     );
   }
 
@@ -73,6 +79,22 @@ class Model {
     return logDataStr.includes(this.filter.keyword);
   }
 
+  matchFilterTimestamp(logTimestamp) {
+    if (!this.filter.timeStamp) {
+      return true;
+    }
+
+    const startTime = this.filter.timeStamp.start;
+    const endTime = this.filter.timeStamp.stop;
+    const logTime = Date.parse(logTimestamp);
+
+    if (logTime < startTime || logTime > endTime) {
+      return false;
+    }
+
+    return true;
+  }
+
   clearLogs() {
     this.logs = [];
   }
@@ -96,6 +118,7 @@ class View {
       'filter-option[filter-key="name"]'
     );
     this.keywordFilter = document.querySelector('filter-keyword');
+    this.timestampFilter = document.querySelector('filter-timestamp');
 
     this.clearLogBtn.addEventListener('click', this);
     this.saveLogBtn.addEventListener('click', this);
@@ -170,6 +193,7 @@ class Controller {
     this.view.viewTypeFilter.addEventListener('filterchange', this);
     this.view.apiTypeFilter.addEventListener('filterchange', this);
     this.view.apiNameFilter.addEventListener('filterchange', this);
+    this.view.timestampFilter.addEventListener('filterchange', this);
 
     browser.runtime.onMessage.addListener((message) => {
       const { requestTo, requestType } = message;

src/lib/web-component/filter-timestamp/filter-timestamp-element.js
@@ -1,0 +1,87 @@
+export class FilterTimestamp extends HTMLElement {
+  constructor() {
+    super();
+    this.timeStamp = null;
+
+    const shadow = this.attachShadow({ mode: 'open' });
+
+    const filterWrapper = document
+      .querySelector('#filterTimestampTemplate')
+      .content.cloneNode(true);
+
+    this.filterContainer = filterWrapper.querySelector(
+      '.timestamp-filter-container'
+    );
+
+    this.startTimeLabel = filterWrapper.querySelector('#startTimeLabel');
+    this.stopTimeLabel = filterWrapper.querySelector('#stopTimeLabel');
+    this.clearBtn = filterWrapper.querySelector('#clearBtn');
+
+    shadow.appendChild(filterWrapper);
+  }
+
+  setFilterRange = (info) => {
+    const selectedEl = browser.menus.getTargetElement(info.targetElementId);
+    const selectedRow = selectedEl.closest('tr');
+
+    if (!selectedRow) {
+      return;
+    }
+
+    // this.timestamp is null when timestamp filter is not applied.
+    if (!this.timeStamp) {
+      this.timeStamp = {};
+    }
+
+    const chosenTimestamp = selectedRow.querySelector('.timestamp').textContent;
+    if (info.menuItemId === 'startTime') {
+      this.timeStamp.start = Date.parse(chosenTimestamp);
+      this.startTimeLabel.textContent = chosenTimestamp;
+    } else if (info.menuItemId === 'stopTime') {
+      this.timeStamp.stop = Date.parse(chosenTimestamp);
+      this.stopTimeLabel.textContent = chosenTimestamp;
+    }
+
+    this.filterContainer.hidden = false;
+    this.dispatchFilterChange();
+  };
+
+  dispatchFilterChange() {
+    const filterDetail = {
+      updateFilter: {
+        timeStamp: this.timeStamp,
+      },
+    };
+
+    this.dispatchEvent(
+      new CustomEvent('filterchange', { detail: filterDetail })
+    );
+  }
+
+  onClearFilter = () => {
+    this.startTimeLabel.textContent = 'From Beginning';
+    this.stopTimeLabel.textContent = 'Up to End';
+    this.filterContainer.hidden = true;
+
+    this.timeStamp = null;
+    this.dispatchFilterChange();
+  };
+
+  onHiddenListener = () => {
+    browser.menus.removeAll();
+  };
+
+  connectedCallback() {
+    browser.menus.onClicked.addListener(this.setFilterRange);
+    browser.menus.onHidden.addListener(this.onHiddenListener);
+    this.clearBtn.addEventListener('click', this.onClearFilter);
+  }
+
+  disconnectedCallback() {
+    browser.menus.onClicked.removeListener(this.setFilterRange);
+    browser.menus.onHidden.removeListener(this.onHiddenListener);
+    this.clearBtn.removeEventListener('click', this.onClearFilter);
+  }
+}
+
+window.customElements.define('filter-timestamp', FilterTimestamp);

src/lib/web-component/filter-timestamp/filter-timestamp.css
@@ -1,0 +1,40 @@
+.timestamp-filter-container .label {
+  width: max-content;
+  display: flex;
+  background: #000;
+  padding: 10px 20px;
+  color: #ffffff;
+  align-items: center;
+}
+
+.timestamp-filter-options {
+  display: flex;
+  justify-content: space-evenly;
+  padding: 5px;
+  border-top: 2px solid #000000;
+  border-left: 2px solid #000000;
+  border-right: 2px solid #000000;
+}
+
+.timestamp-filter-options .title {
+  font-weight: bold;
+}
+
+#clearBtn {
+  cursor: pointer;
+  background: url(/icons/circle-close.svg) no-repeat center center / cover;
+  width: 25px;
+  height: 25px;
+  display: inline-block;
+  margin-left: 15px;
+}
+
+#startTimeLabel,
+#stopTimeLabel {
+  padding-left: 5px;
+}
+
+.start-time-container,
+.stop-time-container {
+  display: flex;
+}

src/lib/web-component/log-view/log-view.css
@@ -76,5 +76,5 @@ table tbody tr {
 table td {
   word-wrap: anywhere;
   text-align: center;
-  padding: 5px;
+  padding: 10px 5px;
 }

commit f398abd27a9cfb13db5aa51e90a49ba6026f5863

fix: space in JSON stringify, regression from PR #17

src/lib/web-component/log-view/log-view-element.js
@@ -81,7 +81,7 @@ export class LogView extends HTMLElement {
   }
 
   openDetailSidebar(logDetails) {
-    const logString = JSON.stringify(logDetails);
+    const logString = JSON.stringify(logDetails, null, 2);
     this.logDetails.textContent = logString;
     this.logTableWrapper.classList.add('width-60');
     this.logDetailWrapper.hidden = false;